7b5f2561f4b31d1049a847989af9ab5aa4c06c28
designermehedi <mehedihasan80912@gmail.com>
2022-12-12 21:25:18 +0600

    color issue fixed

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -12,14 +12,14 @@ const Header = () => {
 
     return (
         <div>    
-            <div className="navbar bg-black lg:fixed lg:container lg:px-32 px-4 lg:text-white">
+            <div className="navbar bg-black lg:fixed lg:container lg:px-32 px-4 text-white">
                 <div className="navbar-start w-full flex justify-between">
                     <a className="btn btn-ghost normal-case text-xl" href="#home">M.Tonmoy</a>
                   <div className="dropdown relative">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden flex">
                       <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                     </label>
-                    <ul tabIndex={0} className="menu absolute right-1 menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-md w-52">
+                    <ul tabIndex={0} className="menu absolute right-1 menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-md w-52 text-black">
                       {menuItems}
                     </ul>
                   </div>
